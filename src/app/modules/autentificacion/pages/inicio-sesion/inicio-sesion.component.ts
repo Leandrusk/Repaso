0f5691431c3d677bd41753f002128e8e3f823065
Leandrusk <Leandrosotocarrasco91@gmail.com>
2024-09-25 13:46:14 -0300

    commit piola

--- src/app/modules/autentificacion/pages/inicio-sesion/inicio-sesion.component.ts
+++ src/app/modules/autentificacion/pages/inicio-sesion/inicio-sesion.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+import { AuthService } from '../../services/auth.service';
+import { FirestoreService } from 'src/app/modules/shared/services/firestore.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -6,5 +11,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio-sesion.component.css']
 })
 export class InicioSesionComponent {
+  hide = true;
 
+  constructor(
+    public servicioAuth: AuthService,
+    public servicioFirestore: FirestoreService,
+    public servicioRutas: Router
+  ) { }
+
+  // Importamos la interfaz de usuario e inicializamos vacío
+  usuarioIngresado: Usuario = {
+    uid: '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    rol: '',
+    password: ''
+  }
+
+  // Función para el inicio de sesión
+  async iniciarSesion() {
+    
+    const credenciales = {
+      email: this.usuarioIngresado.email,
+      password: this.usuarioIngresado.password
+    }
+
+    const res = await this.servicioAuth.iniciarSesion(credenciales.email, credenciales.password)
+      .then(res => {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your work has been saved",
+          showConfirmButton: false,
+          timer: 1500
+        });
+
+        this.servicioRutas.navigate(['/inicio']);
+      })
+      .catch(err => {
+        Swal.fire({
+          icon: "error",
+          title: "Epa...",
+          text: "Te salio algo mal capo!",
+          footer: '<a href="#">Volver al inicio</a>'
+        });
+        this.limpiarInputs();
+      })
+  }
+
+  // Función para vaciar el formulario
+  limpiarInputs() {
+    const inputs = {
+      email: this.usuarioIngresado.email = '',
+      password: this.usuarioIngresado.password = ''
+    }
+  }
 }
--- src/app/modules/autentificacion/pages/registro/registro.component.ts
+++ src/app/modules/autentificacion/pages/registro/registro.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+// Servicio de Autentificación
+import { AuthService } from '../../services/auth.service';
+// Servicio de Firestore
+import { FirestoreService } from 'src/app/modules/shared/services/firestore.service';
+// Servicio de rutas que otorga Angular
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
@@ -6,5 +14,86 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+  // Este "hide" es para el input de contraseña
+  hide = true;
 
+  // IMPORTACIÓN DEL MODELO / INTERFAZ
+  usuarios: Usuario = {
+    uid: '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    rol: '',
+    password: ''
+  }
+
+  // CREAR UNA COLECCIÓN QUE SOLO RECIBE OBJETOS DEL TIPO USUARIOS
+  coleccionUsuarios: Usuario[] = [];
+
+  // Referenciamos a nuestros servicios
+  constructor(
+    public servicioAuth: AuthService, // métodos de autentificación
+    public servicioFirestore: FirestoreService, // vincula UID con la colección
+    public servicioRutas: Router // método de navegación
+  ){}
+
+  // FUNCIÓN ASINCRONICA PARA EL REGISTRO
+  async registrar(){
+    const credenciales = {
+      email: this.usuarios.email,
+      password: this.usuarios.password
+    }
+
+    // constante "res" = resguarda una respuesta
+    const res = await this.servicioAuth.registrar(credenciales.email, credenciales.password)
+    // El método THEN nos devuelve la respuesta esperada por la promesa
+    .then(res => {
+      alert('Ha agregado un usuario con éxito :)');
+
+      // Accedemos al servicio de rutas -> método navigate
+      // método NAVIGATE = permite dirigirnos a diferentes vistas
+      this.servicioRutas.navigate(['/inicio']);
+    })
+    // El método CATCH toma una falla y la vuelve un ERROR
+    .catch(error => {
+      Swal.fire({
+        icon: "error",
+        title: "Epa...",
+        text: "Te salio algo mal capo!",
+        footer: '<a href="#">Volver al inicio</a>'
+      });
+    })
+
+    const uid = await this.servicioAuth.obtenerUid();
+
+    this.usuarios.uid = uid;
+
+    this.guardarUsuario();
+
+    // Llamamos a la función limpiarInputs() para que se ejecute
+    this.limpiarInputs();
+  }
+
+  // función para agregar NUEVO USUARIO
+  async guardarUsuario(){
+    this.servicioFirestore.agregarUsuario(this.usuarios, this.usuarios.uid)
+    .then(res => {
+      console.log(this.usuarios);
+    })
+    .catch(err => {
+      console.log('Error =>', err);
+    })
+  }
+
+  // Función para vaciar el formulario
+  limpiarInputs(){
+    const inputs = {
+      uid: this.usuarios.uid = '',
+      nombre: this.usuarios.nombre = '',
+      apellido: this.usuarios.apellido = '',
+      email: this.usuarios.email = '',
+      rol: this.usuarios.rol = '',
+      password: this.usuarios.password = ''
+    }
+  }
 }
