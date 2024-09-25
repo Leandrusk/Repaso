cde1144ca49da19c5861ece593e9d9380a38498d
Leandrusk <Leandrosotocarrasco91@gmail.com>
2024-09-25 19:46:28 -0300

    Creación de los componentes del inicio,carrusel piola y un par de cards

--- src/app/modules/inicio/components/card/card.component.ts
+++ src/app/modules/inicio/components/card/card.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
-
+//IMPORTAMOS LA INTERFAZ PARA CIUDAD
+import { Libro } from 'src/app/models/libro';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  //PROPIEDAD PUBLICA --> TIPO ARRAY
+  public info: Libro[];
+
+  //INICIALIZAR LA PROPIEDAD INFO
+  constructor() {
+    this.info = [
+      {
+        id: "",
+        titulo: "And the there were none",
+        genero: "terror",
+        portada: "https://m.media-amazon.com/images/I/81nChcVy7CL._AC_UF1000,1000_QL80_.jpg",
+        mostrarInfo: false
+
+      },
+
+      {
+        id: "",
+        titulo: "el principito",
+        genero: "ficcion",
+        portada: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDTpwoDBsM9mKzjKSZS7Ke99z1cJboypiPfQ&s",
+        mostrarInfo: false
+      },
+
+      {
+        id: "",
+        titulo: "alicia en el pais de las maravillas",
+        genero: "Fantasia",
+        portada: "https://images.cdn1.buscalibre.com/fit-in/360x360/d0/0a/d00af29b874c7ab6a7bbd56f246d92f8.jpg",
+        mostrarInfo: false
+      },
+    ]
+  }
+
+  
+  mostrarInfo(item: Libro) {
+    item.mostrarInfo = true;
+  }
+
+  ocultarInfo(item: Libro) {
+    item.mostrarInfo = false;
+  }
+
+  comprar() {
+    alert('¡DEBES INICIAR SESION PARA PODER COMPRAR!');
+  }
 
 }
--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -2,20 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
-import { CardComponent } from './components/card/card.component';
+
+//vista - pagina - interfaz del usuario
 import { InicioComponent } from './pages/inicio/inicio.component';
+//componentes locales
+import { CardComponent } from './components/card/card.component';
 
-
-
+//componentes de material
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    CardComponent,
-    InicioComponent
+    InicioComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
-    InicioRoutingModule
+    InicioRoutingModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  exports:[
+    MatButtonModule,
+    MatCardModule,
   ]
 })
 export class InicioModule { }
